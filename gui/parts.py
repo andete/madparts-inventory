9fe86f8a81e85db69cbc7e20417480e324133a0a
Joost Yervante Damad <joost@damad.be>
2013-08-30 09:21:12 +0200

    introduce context menu and add part skeleton

--- gui/parts.py
+++ gui/parts.py
@@ -46,6 +46,17 @@ class PartTree(QtGui.QTreeView):
     self.selection_model.currentRowChanged.connect(self.row_changed)
     self.setRootIsDecorated(False)
     self.setSelectionModel(self.selection_model)
+    self.setContextMenuPolicy(QtCore.Qt.ActionsContextMenu)
+    def _add(text, slot = None):
+      action = QtGui.QAction(text, self)
+      self.addAction(action)
+      if slot != None: action.triggered.connect(slot)
+      else: action.setDisabled(True)
+    _add("&Add Category", parent.add_cat)
+    sep = QtGui.QAction(self)
+    sep.setSeparator(True)
+    self.addAction(sep)
+    _add("&Add Part", parent.add_part)
 
   def row_changed(self, current, previous):
     (t,n) = current.data(QtCore.Qt.UserRole)
